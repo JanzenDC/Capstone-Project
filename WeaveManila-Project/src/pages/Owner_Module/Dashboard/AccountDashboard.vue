17ecb2b724d3fb0ef618e04a442a09eee22cf4cb
JanzenDC <janzendelacruz28@gmail.com>
2024-01-30 21:01:59 +0800

    Newly updated drawer design.

--- WeaveManila-Project/src/pages/Owner_Module/Dashboard/AccountDashboard.vue
+++ WeaveManila-Project/src/pages/Owner_Module/Dashboard/AccountDashboard.vue
@@ -4,20 +4,27 @@ show-if-above
 v-model="drawer"
 side="left"
 bordered
-:width="300">
-  <ul class="p-2 flex flex-col h-full static">
+:width="drawerWidth">
+  <ul class="p-2 flex flex-col h-full static" v-if="drawerWidth !== 80">
     <div @click="toggleDrawer" class="absolute -right-4 top-4 text-[18px] bg-white drop-shadow-lg rounded-full px-2 py-1 text-center cursor-pointer">
-      <q-icon name="arrow_back_ios_new"/>
+      <q-icon :name="drawerIcon"/>
     </div>
     <div class="flex">
-      <div class="w-1/4 items-center flex justify-center">
+      <div class="w-1/4 items-center flex justify-center" >
         <q-img
           src="../../../assets/favicon-128x128.png"
           alt="Description of the image"
           class="w-[50px] md:w-[60px]"
         />
       </div>
-        <div class="text-[#281c0f] w-3/4">
+      <div class=" items-center flex justify-center" v-if="drawerWidth <= 80">
+        <q-img
+          src="../../../assets/favicon-128x128.png"
+          alt="Description of the image"
+          class="w-[150px] md:w-[60px]"
+        />
+      </div>
+        <div class="text-[#281c0f] w-3/4" v-if="drawerWidth !== 80">
           <span class=" font-bold text-[20px]">WEAVEMANILA INC.</span><br>
           <span class="text-[#281c0f] text-[12px]">Production Monitoring & Inventory Management System</span>
 
@@ -27,19 +34,19 @@ bordered
 
 
     <li class="font-bold">Overview</li>
-      <li class="py-[10px] px-[20px]">
+      <li class="py-[10px] px-[20px]" >
         <div class="flex items-center">
           <router-link to="/dashboard/main-dashboard">
-          <q-icon name="dashboard" class="mr-2"/> Dashboard
+          <q-icon name="dashboard" class="mr-2" /> <span >Dashboard</span>
           </router-link>
         </div>
       </li>
       <!-- Process Section -->
-      <li class="font-bold">Process</li>
+      <li class="font-bold" >Process</li>
       <li class="py-[10px] px-[20px]" @click="toggleInventoryMenu">
         <div class="flex items-center gap-2 justify-between">
-          <div><q-icon name="inventory"/> Inventory</div>
-          <div><q-icon name="expand_more"/> </div>
+          <div><q-icon name="inventory"/> <span >Inventory</span></div>
+          <div><q-icon name="expand_more"  /> </div>
         </div>
         <ul v-if="inventoryMenuVisible">
           <li class="py-[2px] px-[40px] mt-3">Purchase Order</li>
@@ -49,40 +56,40 @@ bordered
 
       <li class="py-[10px] px-[20px] ">
         <div class="flex items-center gap-2">
-          <i class="bi bi-box-seam"></i> Product Monitoring
+          <i class="bi bi-box-seam"></i> <span >Product Monitoring</span>
         </div>
       </li>
       <li class="py-[10px] px-[20px]">
         <div class="flex items-center">
-          <q-icon name="description" class="mr-2"/> Production Cost Report
+          <q-icon name="description" class="mr-2"/><span >Production Cost Report</span>
         </div>
       </li>
       <!-- Settings Section -->
-      <li class="font-bold">Settings</li>
+      <li class="font-bold" >Settings</li>
       <li class="py-[10px] px-[20px]">
         <div class="flex items-center">
           <router-link to="/dashboard/auditlogs-section">
-            <i class="bi bi-activity mr-2"></i> Audit Logs
+            <i class="bi bi-activity mr-2"></i> <span >Audit Logs</span>
           </router-link>
         </div>
       </li>
       <li class="py-[10px] px-[20px]">
         <div class="flex items-center">
           <router-link to="/dashboard/usermanagement-section">
-            <q-icon name="group" class="mr-2"/> User Management
+            <q-icon name="group" class="mr-2"/> <span >User Management</span>
           </router-link>
         </div>
       </li>
       <li class="py-[10px] px-[20px]">
         <div class="flex items-center">
           <router-link to="/dashboard/account-settings">
-            <q-icon name="manage_accounts" class="mr-2"/> Account Settings
+            <q-icon name="manage_accounts" class="mr-2"/> <span >Account Settings</span>
           </router-link>
         </div>
       </li>
       <li class="mt-auto py-[10px]">
-        <div class="flex justify-between">
-          <div class="flex items-center">
+        <div class="flex justify-between text-center" >
+          <div class="flex items-center" >
             <q-img
               :src="getUserProfileImagePath()"
               alt="Description of the image"
@@ -97,40 +104,96 @@ bordered
               </div>
             </div>
           </div>
-          <div class="flex items-center">
+          <div class="flex items-center ">
             <router-link @click="logout" to="/">
               <q-icon name="logout" class="h-[18px] w-[20px] font-bold"/>
             </router-link>
           </div>
         </div>
       </li>
-    </ul>
-</q-drawer>
-<q-page class="bg-[#f5f5f5] p-4">
-  <div class="h-[620px] rounded p-4 px-7 overflow-y-auto overflow-x-hidden">
-    <div class="flex justify-between items-center">
-      <div class="text-[30px]">
-        <div class="items-center flex">
-          <q-icon
-            name="menu"
-            v-if="showMenuIcon"
-            @click="toggleDrawer"
-            class="cursor-pointer"
-          />
-          <q-icon
-            name="menu"
-            v-if="!showMenuIcon"
-            @click="toggleDrawer"
-            class="cursor-pointer min-[360px]:flex md:hidden"
-          />
-          <span class="font-bold">Account Settings</span>
-        </div>
+  </ul>
+
+  <ul class="p-2 flex flex-col h-full static" v-if="drawerWidth <= 80">
+    <div @click="toggleDrawer" class="absolute -right-4 top-4 text-[18px] bg-white drop-shadow-lg rounded-full px-2 py-1 text-center cursor-pointer">
+      <q-icon :name="drawerIcon"/>
+    </div>
+    <div class="flex">
+      <div class=" items-center flex justify-center" v-if="drawerWidth <= 80">
+        <q-img
+          src="../../../assets/favicon-128x128.png"
+          alt="Description of the image"
+          class="w-[150px] md:w-[60px]"
+        />
       </div>
     </div>
-    <div class="w-full md:flex md:justify-center mt-3">
+
+
+      <li class="py-[10px] px-[20px]" >
+        <div class="flex items-center" @click="toggleDrawer">
+          <q-icon name="dashboard" />
+        </div>
+      </li>
+      <!-- Process Section -->
+
+      <li class="py-[10px] px-[20px]" @click="toggleDrawer">
+        <div class="flex items-center gap-2 justify-between">
+          <div><q-icon name="inventory"/></div>
+        </div>
+      </li>
+
+      <li class="py-[10px] px-[20px] ">
+        <div class="flex items-center gap-2" @click="toggleDrawer">
+          <i class="bi bi-box-seam"></i>
+        </div>
+      </li>
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center" @click="toggleDrawer">
+          <q-icon name="description" />
+        </div>
+      </li>
+      <!-- Settings Section -->
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center" @click="toggleDrawer">
+            <i class="bi bi-activity"></i>
+        </div>
+      </li>
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center" @click="toggleDrawer">
+            <q-icon name="group" class="mr-2"/>
+        </div>
+      </li>
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center" @click="toggleDrawer">
+            <q-icon name="manage_accounts"/>
+        </div>
+      </li>
+      <li class="mt-auto py-[10px]">
+          <div class="flex justify-center ">
+            <router-link @click="logout" to="/">
+              <q-icon name="logout" class="h-[18px] w-[20px] font-bold"/>
+            </router-link>
+          </div>
+      </li>
+  </ul>
+</q-drawer>
+<q-page class="bg-[#f5f5f5] p-4">
+  <div class="h-[620px] rounded p-4 px-7">
+    <div class="">
+      <div class="text-[30px] flex items-center min-[360px]:gap-5">
+        <q-icon
+          name="menu"
+          @click="toggleDrawer"
+          class="cursor-pointer text-[20px] min-[360px]:flex md:hidden "
+        />
+          <span class="font-bold">Account Settings</span>
+      </div>
+      <div class="text-[16px]">Setup you account, edit profile details & change password</div>
+
+    </div>
+    <div class="w-full md:flex md:justify-center mt-3 overflow-y-auto overflow-x-hidden h-[500px]">
     <div>
         <!-- User Area -->
-        <div class="p-4 md:w-[600px] h-[150px] border border-[#ddb7ab] rounded-[15px] drop-shadow-md flex md:justify-between
+        <div class="p-4 md:w-[600px] h-[130px] border border-[#ddb7ab] rounded-[15px] drop-shadow-md flex md:justify-between
         min-[390px]:justify-center">
           <div class="md:flex items-center gap-2">
             <div>
@@ -326,8 +389,10 @@ export default {
       arrowDirection: false,
       showModal: false,
       fullnames: '',
+      toggleDrawers: true,
       drawer: false,
-      showMenuIcon: false,
+      drawerWidth: 300,
+      drawerIcon: 'arrow_back_ios',
       inventoryMenuVisible: false,
       statusCheckTimer: null,
       editProfile: false,
@@ -347,6 +412,19 @@ export default {
   },
 
   methods: {
+    toggleDrawer() {
+      if (!this.toggleDrawers) {
+        this.drawer = true;
+        this.drawerWidth = 300;
+        this.drawerIcon = 'arrow_forward_ios';
+        this.toggleDrawers = true;
+      } else {
+        this.drawer = true;
+        this.drawerWidth = 80;
+        this.toggleDrawers = false;
+        this.drawerIcon = 'arrow_back_ios_new';
+      }
+    },
     checkUserStatus() {
         axios.get(`http://localhost/Capstone-Project/backend/api/verification.php?email=${this.email}`)
         .then(response => {
@@ -442,10 +520,6 @@ export default {
     },
     toggleInventoryMenu() {
       this.inventoryMenuVisible = !this.inventoryMenuVisible;
-    },
-    toggleDrawer() {
-      this.drawer = !this.drawer;
-      this.showMenuIcon = !this.showMenuIcon;
     },
     toggleModal() {
       this.arrowDirection = !this.arrowDirection;
