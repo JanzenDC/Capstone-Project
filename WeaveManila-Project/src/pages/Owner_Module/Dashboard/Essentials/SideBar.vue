9aef94ba06b3b4705e39ced4104aa08a36474d81
Janziiin <janzendelacruz28@gmail.com>
2024-04-23 11:22:02 +0800

    - Changes in sidebar - Move account settings to top ( Main Dashboard, Account. Audit, Backup, Usermanagement )

---
+++ WeaveManila-Project/src/pages/Owner_Module/Dashboard/Essentials/SideBar.vue
@@ -0,0 +1,283 @@
+<template>
+<q-drawer
+show-if-above
+v-model="drawer"
+side="left"
+bordered
+:width="drawerWidth">
+  <ul class=" p-2 flex flex-col h-full static" v-if="drawerWidth !== 80">
+    <div class="flex">
+      <div class="w-1/4 items-center flex justify-center" >
+        <q-img
+          src="~assets/favicon-128x128.png"
+          alt="Description of the image"
+          class="w-[50px] md:w-[60px]"
+        />
+      </div>
+      <div class=" items-center flex justify-center" v-if="drawerWidth <= 80">
+        <q-img
+          src="~assets/favicon-128x128.png"
+          alt="Description of the image"
+          class="w-[150px] md:w-[60px]"
+        />
+      </div>
+      <div class="text-[#281c0f] text-[18px] w-3/4 flex justify-center items-center gap-1" v-if="drawerWidth !== 80">
+      <div>
+        <span class="font-bold ">WEAVEMANILA INC.</span><br>
+      </div>
+      <div>
+        <q-icon name="keyboard_double_arrow_right" class="text-[30px] cursor-pointer" @click="toggleDrawer" />
+      </div>
+    </div>
+
+    </div>
+
+
+
+    <li class="font-bold mt-5">Menu</li>
+      <li class="py-[10px] px-[20px]" >
+        <div class="flex items-center">
+          <router-link to="/dashboard/main-dashboard">
+          <q-icon name="dashboard" class="mr-2" /> <span >Dashboard</span>
+          </router-link>
+        </div>
+      </li>
+
+      <li class="py-[10px] px-[20px]" @click="toggleInventoryMenu">
+        <div class="flex items-center cursor-pointer gap-2 justify-between">
+          <div><q-icon name="inventory"/> <span >Inventory</span></div>
+          <div><q-icon name="expand_more"  /> </div>
+        </div>
+        <ul v-if="inventoryMenuVisible">
+          <router-link to="/dashboard/inventory-section">
+            <li class="py-[2px] px-[40px] mt-3">Materials</li>
+          </router-link>
+          <router-link to="/dashboard/mpoform-section">
+            <li class="px-[40px] mt-3"> Material Purchase Order</li>
+          </router-link>
+        </ul>
+      </li>
+
+      <li class="py-[10px] px-[20px]" @click="toggleProduction">
+        <div class="flex items-center cursor-pointer gap-2 justify-between">
+          <div>
+            <i class="bi bi-box-seam"></i> <span >Product Monitoring</span>
+          </div>
+          <div><q-icon name="expand_more"  /> </div>
+        </div>
+        <ul v-if="productionVisible">
+          <router-link to="/dashboard/productionplan-section">
+            <li class="py-[2px] px-[40px] mt-3">Production Plan</li>
+          </router-link>
+        </ul>
+      </li>
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center">
+          <q-icon name="assignment_add" class="mr-2"/><span>Report</span>
+        </div>
+      </li>
+      <!-- Settings Section -->
+      <li class="font-bold" >Admin</li>
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center">
+          <router-link to="/dashboard/auditlogs-section">
+            <i class="bi bi-activity mr-2"></i> <span >Audit Logs</span>
+          </router-link>
+        </div>
+      </li>
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center">
+          <router-link to="/dashboard/usermanagement-section">
+            <q-icon name="group" class="mr-2"/> <span >User Management</span>
+          </router-link>
+        </div>
+      </li>
+      <li class="py-[10px] px-[20px]" v-if="isAdmin === 1">
+        <div class="flex items-center">
+          <router-link to="/dashboard/backup-section">
+            <q-icon name="backup" class="mr-2"/> <span >Data Backup</span>
+          </router-link>
+        </div>
+      </li>
+
+  </ul>
+
+  <ul class="p-2 flex flex-col h-full static" v-if="drawerWidth <= 80">
+
+    <div class="flex justify-center items-center text-[40px]">
+      <div class=" items-center flex justify-center cursor-pointer" v-if="drawerWidth <= 80">
+        <q-icon name="keyboard_double_arrow_right" @click="toggleDrawer"/>
+      </div>
+    </div>
+
+      <li class="mt-5 text-center">Menu</li>
+      <li class="py-[10px] px-[20px]" >
+        <div class="flex items-center" @click="toggleDrawer">
+          <q-icon name="dashboard" />
+        </div>
+      </li>
+      <!-- Process Section -->
+
+      <li class="py-[10px] px-[20px]" @click="toggleDrawer">
+        <div class="flex items-center cursor-pointer gap-2 justify-between">
+          <div><q-icon name="inventory"/></div>
+        </div>
+      </li>
+
+      <li class="py-[10px] px-[20px] ">
+        <div class="flex items-center gap-2" @click="toggleDrawer">
+          <i class="bi bi-box-seam"></i>
+        </div>
+      </li>
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center" @click="toggleDrawer">
+          <q-icon name="description" />
+        </div>
+      </li>
+      <li class="text-center">Admin</li>
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center" @click="toggleDrawer">
+            <i class="bi bi-activity"></i>
+        </div>
+      </li>
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center" @click="toggleDrawer">
+            <q-icon name="group" class="mr-2"/>
+        </div>
+      </li>
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center" @click="toggleDrawer">
+            <q-icon name="manage_accounts"/>
+        </div>
+      </li>
+      <li class="py-[10px] px-[20px]">
+        <div class="flex items-center" @click="toggleDrawer">
+            <q-icon name="backup"/>
+        </div>
+      </li>
+
+
+
+  </ul>
+</q-drawer>
+
+</template>
+
+<script>
+import { useQuasar } from 'quasar';
+import { SessionStorage } from 'quasar';
+
+export default {
+  setup() {
+    const $q = useQuasar();
+  },
+  data() {
+    return {
+      isAdmin: 0,
+      email: '',
+      fullname: '',
+      firstname: '',
+      middlename: '',
+      lastname: '',
+      userProfileImage: null,
+      username: '',
+      position: '',
+      status: '',
+      drawer: false,
+      showMenuIcon: false,
+      statusCheckTimer: null,
+      toggleDrawers: true,
+      drawerWidth: 300,
+      drawerIcon: 'arrow_back_ios',
+      inventoryMenuVisible: false,
+      productionVisible: false,
+      OpenLogout: false,
+      modalVisible: false,
+    };
+  },
+  beforeUnmount() {
+    clearInterval(this.statusCheckTimer);
+  },
+  mounted() {
+    this.loadUserData();
+  },
+  methods: {
+    loadUserData() {
+      const userData = SessionStorage.getItem('information');
+
+      if (userData) {
+        try {
+          const userInformation = JSON.parse(userData);
+          this.email = userInformation.email;
+          this.username = userInformation.username;
+          this.userProfileImage = userInformation.pfp;
+          this.firstname = userInformation.firstname;
+          this.middlename = userInformation.middlename;
+          this.lastname = userInformation.lastname;
+          this.position = userInformation.position;
+          this.status = userInformation.status;
+          this.isAdmin = userInformation.isAdmin;
+          this.fullname = this.firstname + " " + this.lastname;
+          if (this.position.toLowerCase() === 'owner') {
+
+            this.$router.push('/dashboard/auditlogs-section');
+          } else {
+
+            this.$q.notify({
+              type: 'negative',
+              message: 'You do not have permission to access the system.',
+            });
+            this.$router.push('/');
+            sessionStorage.clear();
+          }
+
+          if (this.status === 0 && !this.isAdmin) {
+            this.$q.notify({
+              type: 'negative',
+              message: 'Your account is currently inactive. Please contact the administrator.',
+            });
+            this.$router.push('/');
+            sessionStorage.clear();
+          }
+
+        } catch (error) {
+          console.log('Error parsing user data:', error);
+          // Provide user feedback or navigate to an error page
+          this.$q.notify({
+            type: 'negative',
+            message: 'Error loading user data. Please try again.',
+          });
+          this.$router.push('/');
+          sessionStorage.clear();
+        }
+      } else {
+        // Handle the case when user data is not available
+        this.$router.push('/');
+        sessionStorage.clear();
+      }
+    },
+    toggleModal() {
+      this.modalVisible = !this.modalVisible;
+    },
+    toggleProduction(){
+      this.productionVisible = !this.productionVisible;
+    },
+    toggleInventoryMenu() {
+      this.inventoryMenuVisible = !this.inventoryMenuVisible;
+    },
+    toggleDrawer() {
+      if (!this.toggleDrawers) {
+        this.drawer = true;
+        this.drawerWidth = 300;
+        this.drawerIcon = 'arrow_forward_ios';
+        this.toggleDrawers = true;
+      } else {
+        this.drawer = true;
+        this.drawerWidth = 80;
+        this.toggleDrawers = false;
+        this.drawerIcon = 'arrow_back_ios_new';
+      }
+    },
+  },
+};
+</script>    
